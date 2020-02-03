c77ebd764ad39e9d881e13d0236e0914750c8318
Aidan Weber-Concannon <46550819+aidanwc@users.noreply.github.com>
2020-02-02 20:35:08 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template,redirect,url_for
+app= Flask(__name__)
+
+@app.route('/<name>')
+def home(name):
+    return render_template("index.html",content=name)
+
+
+
+if __name__=="__main__":
+    app.run()
